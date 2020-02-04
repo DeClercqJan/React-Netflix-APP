fe46f509eb97dd1086bca5350f84f0cf63b464d3
jan <declercqjan@gmail.com>
2020-02-04 14:34:50 +0100

    getting closer. Set up dummy initital movie list and getting api to call fixed list of movies to replace it with (title property only)

--- src/App.js
+++ src/App.js
@@ -5,14 +5,11 @@ import Axios from "axios";
 function App() {
   // note: need to write this with starting capital letter (at the very least if you want to use it as a component)
   function GetMovies() {
-    const inititalMovies = [
-      {
-        title: "movie1"
-      },
-      {
-        title: "movie2"
-      }
-    ];
+    const inititalMovies = {
+        results : [{
+          title: "test1"
+        }]
+    }
 
     const [Movies, setMovies] = useState(inititalMovies);
     const [SearchText, setSearchText] = useState(
@@ -28,7 +25,7 @@ function App() {
       console.log(event);
       if (SearchText) {
         Axios.get(
-          `https://api.themoviedb.org/3/movie/550?api_key=bde60eb3d70191bf80d726a2da4ae238`
+          `https://api.themoviedb.org/3/search/movie?api_key=bde60eb3d70191bf80d726a2da4ae238&language=en-US&query=boys&page=1&include_adult=false`
         ).then(res => {
           const movies = res.data;
           setMovies(movies);
@@ -36,6 +33,13 @@ function App() {
       }
       setSearchText("");
       event.preventDefault();
+    };
+
+    const MoviesDisplayed = () => {
+      console.log({Movies});
+      let MoviesList = {Movies};
+      console.log(MoviesList);
+      return (MoviesList.Movies.results[0].title);
     };
 
     return (
@@ -53,7 +57,7 @@ function App() {
           </form>
         </div>
         <div>
-          <p>{Movies.title}</p>
+          <MoviesDisplayed/>
         </div>
       </Fragment>
     );
