aa7d7afca7ee30ef1998810e94f6f4d0c62d7b3f
jan <declercqjan@gmail.com>
2020-02-04 12:44:26 +0100

    commented out some lines that worked previously as I'm moving towards a form with submit but decided to take it step for step as I progress

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { useState } from "react";
+// import Axios from "axios";
 // import "./App.css";
 
 function App() {
   // note: need to write this with starting capital letter (at the very least if you want to use it as a component)
-  function GetMoviesSetState() {
+  function GetMovies() {
     // Declare a new state variable, which we'll call "count"
-    const [count, setCount] = useState(0);
-  
+    // const [Movies, setMovies] = useState([]);
+    const [Search, setSearch] = useState("no movie searched");
+
+    const handleChange = event => {
+      console.log(event.target.value);
+      setSearch(event.target.value);
+    };
+
+/*     const handleMovieSearch = event2 => {
+      // console.log(Search);
+      console.log(event2);
+      Axios.get(
+        `https://api.themoviedb.org/3/movie/550?api_key=bde60eb3d70191bf80d726a2da4ae238`
+      ).then(res => {
+        const movies = res.data;
+        setMovies(movies);
+      });
+      event2.preventDefault();
+    }; */
+
     return (
       <div>
-        <p>You clicked {count} times</p>
-        <button onClick={() => setCount(count + 1)}>
-          Click me
-        </button>
+        {/* <p>You clicked {Movies[0]} times</p> */}
+        {/* function for form needs to on form element */}
+        {/* <form onSubmit={handleMovieSearch}> */}
+        <input type="search" value={Search} onChange={handleChange}></input>
+        {/* <input type="submit">Click me</input> */}
+        {/* </form> */}
       </div>
     );
   }
@@ -20,7 +41,7 @@ function App() {
   return (
     <div>
       <p>test</p>
-      <GetMoviesSetState/>
+      <GetMovies />
     </div>
   );
 }
