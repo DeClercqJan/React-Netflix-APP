ec20e3cce709a6df9bfb8de4c124fcd2a924c66b
jan <declercqjan@gmail.com>
2020-02-04 14:38:29 +0100

    able to dynamically search api and display title of first movie in respons array

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
   // note: need to write this with starting capital letter (at the very least if you want to use it as a component)
   function GetMovies() {
     const inititalMovies = {
-        results : [{
+      results: [
+        {
           title: "test1"
-        }]
-    }
+        }
+      ]
+    };
 
     const [Movies, setMovies] = useState(inititalMovies);
     const [SearchText, setSearchText] = useState(
@@ -24,8 +26,10 @@ function App() {
     const handleMovieSearch = event => {
       console.log(event);
       if (SearchText) {
+        const key = "bde60eb3d70191bf80d726a2da4ae238";
+        let query = SearchText;
         Axios.get(
-          `https://api.themoviedb.org/3/search/movie?api_key=bde60eb3d70191bf80d726a2da4ae238&language=en-US&query=boys&page=1&include_adult=false`
+          `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=${query}&page=1&include_adult=false`
         ).then(res => {
           const movies = res.data;
           setMovies(movies);
@@ -36,10 +40,10 @@ function App() {
     };
 
     const MoviesDisplayed = () => {
-      console.log({Movies});
-      let MoviesList = {Movies};
+      console.log({ Movies });
+      let MoviesList = { Movies };
       console.log(MoviesList);
-      return (MoviesList.Movies.results[0].title);
+      return MoviesList.Movies.results[0].title;
     };
 
     return (
@@ -57,7 +61,7 @@ function App() {
           </form>
         </div>
         <div>
-          <MoviesDisplayed/>
+          <MoviesDisplayed />
         </div>
       </Fragment>
     );
