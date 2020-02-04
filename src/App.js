3c32695b2c17f4dec4eecbfacdb65fa7e41e058a
jan <declercqjan@gmail.com>
2020-02-04 14:04:06 +0100

    basic structure in order to get and display stuff. Much of the stuff is still placeholder stuff.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,61 @@
-import React, { useState } from "react";
-// import Axios from "axios";
+import React, { useState, Fragment } from "react";
+import Axios from "axios";
 // import "./App.css";
 
 function App() {
   // note: need to write this with starting capital letter (at the very least if you want to use it as a component)
   function GetMovies() {
-    // Declare a new state variable, which we'll call "count"
-    // const [Movies, setMovies] = useState([]);
-    const [Search, setSearch] = useState("no movie searched");
+    const inititalMovies = [
+      {
+        title: "movie1"
+      },
+      {
+        title: "movie2"
+      }
+    ];
 
-    const handleChange = event => {
+    const [Movies, setMovies] = useState(inititalMovies);
+    const [SearchText, setSearchText] = useState(
+      "enter keywords to find movies"
+    );
+
+    const handleSearchText = event => {
       console.log(event.target.value);
-      setSearch(event.target.value);
+      setSearchText(event.target.value);
     };
 
-/*     const handleMovieSearch = event2 => {
-      // console.log(Search);
-      console.log(event2);
-      Axios.get(
-        `https://api.themoviedb.org/3/movie/550?api_key=bde60eb3d70191bf80d726a2da4ae238`
-      ).then(res => {
-        const movies = res.data;
-        setMovies(movies);
-      });
-      event2.preventDefault();
-    }; */
+    const handleMovieSearch = event => {
+      console.log(event);
+      if (SearchText) {
+        Axios.get(
+          `https://api.themoviedb.org/3/movie/550?api_key=bde60eb3d70191bf80d726a2da4ae238`
+        ).then(res => {
+          const movies = res.data;
+          setMovies(movies);
+        });
+      }
+      setSearchText("");
+      event.preventDefault();
+    };
 
     return (
-      <div>
-        {/* <p>You clicked {Movies[0]} times</p> */}
-        {/* function for form needs to on form element */}
-        {/* <form onSubmit={handleMovieSearch}> */}
-        <input type="search" value={Search} onChange={handleChange}></input>
-        {/* <input type="submit">Click me</input> */}
-        {/* </form> */}
-      </div>
+      <Fragment>
+        <div>
+          {/* <p>You clicked {Movies[0]} times</p> */}
+          {/* function for form needs to on form element */}
+          <form onSubmit={handleMovieSearch}>
+            <input
+              type="search"
+              value={SearchText}
+              onChange={handleSearchText}
+            ></input>
+            <button type="submit">Click Me</button>
+          </form>
+        </div>
+        <div>
+          <p>{Movies.title}</p>
+        </div>
+      </Fragment>
     );
   }
 
