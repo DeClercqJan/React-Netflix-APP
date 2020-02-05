7df6311104e162d4929c329bbeecef2c84863540
jan <declercqjan@gmail.com>
2020-02-05 15:38:11 +0100

    able to display movie posters

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,13 @@ import React from "react";
 
 function Card(props) {
   const movieDataLower = props.movieDataLower;
+  console.log(movieDataLower);
   return (
     <article>
       <h3 key={movieDataLower.id}>{movieDataLower.title}</h3>
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movieDataLower.poster_path}`}
+      ></img>
       <p>{movieDataLower.overview}</p>
     </article>
   );
