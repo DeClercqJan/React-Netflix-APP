6ed667995ce4c940edf30f2772cf37323a95809e
jan <declercqjan@gmail.com>
2020-02-06 15:07:14 +0100

    added component that fetches moviedetails. need to clean up router mess

--- src/view/Home.js
+++ src/view/Home.js
@@ -7,12 +7,13 @@ import {
   Switch,
   Route,
   Link,
-  useRouteMatch,
+  // useRouteMatch,
   useParams
 } from "react-router-dom";
 // import "./App.css";
 
 // fundamental remark: this would be way cooler if I configured it to represent my own preferences and in a click of a button I get a recommendation for how I'm feeling that moment (love sad, wanting to party etc.). Also, should not have seen the before
+// maybe better instead of moviedetails: jeroen had good idea: get youtube key from https://developers.themoviedb.org/3/movies/get-movie-videos and set as parameter inhttps://www.youtube.com/watch?v=UcmZN0Mbl04 probably will be method changing
 
 // note: need to write this with starting capital letter (at the very least if you want to use it as a component)
 function Home() {
@@ -55,53 +56,6 @@ function Home() {
     console.log("use effect fires");
   });
 
-  function MovieDetails() {
-    console.log("movieDetails functies firet");
-    const dummy = MoviesHigher.results[0].id;
-    console.log(dummy);
-    return dummy;
-    // return (<p>testje Moviedetails</p>);
-  }
-
-  function TestParameters() {
-    let match = useRouteMatch();
-  
-    return (
-      <div>
-        <h2>Topics</h2>
-  
-        <ul>
-          <li>
-            <Link to={`${match.url}/components`}>Components</Link>
-          </li>
-          <li>
-            <Link to={`${match.url}/props-v-state`}>
-              Props v. State
-            </Link>
-          </li>
-        </ul>
-  
-        {/* The Topics page has its own <Switch> with more routes
-            that build on the /topics URL path. You can think of the
-            2nd <Route> here as an "index" page for all topics, or
-            the page that is shown when no topic is selected */}
-        <Switch>
-          <Route path={`${match.path}/:topicId`}>
-            <Topic />
-          </Route>
-          <Route path={match.path}>
-            <h3>Please select a topic.</h3>
-          </Route>
-        </Switch>
-      </div>
-    );
-  }
-  
-  function Topic() {
-    let { topicId } = useParams();
-    return <h3>Requested topic ID: {topicId}</h3>;
-  }
-
   return (
     <div className="container-fluid">
       <header className="row">
@@ -113,20 +67,11 @@ function Home() {
           handleMovieSearchHigher={handleMovieSearchHigher}
         />
         <Router>
-          <Link to="/movieDetails">Movie details</Link>
-          <Link to="/moviesList">List of movies</Link>
           <Link to="/testParameters">testParameters</Link>
           <Switch>
-            <Route path="/movieDetails">
-              <MovieDetails />
-            </Route>
-          </Switch>
-          <Switch>
-            <Route path="/moviesList">
-              <Results MoviesLower={MoviesHigher} />
-            </Route>
             <Route path="/testParameters">
-              <TestParameters/>
+              {/* <TestParameters/> */}
+              <Results MoviesLower={MoviesHigher} />
             </Route>
           </Switch>
         </Router>
--- src/view/Results.js
+++ src/view/Results.js
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
 import Card from "../components/Card";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import Axios from "axios";
 
 function Results(props) {
+  let match = useRouteMatch();
+
   const MoviesLower = props.MoviesLower;
   const moviesListUnformated = MoviesLower.results;
   const moviesListFormated = moviesListUnformated.map(movieDataHigher => {
-    return <Card movieDataLower={movieDataHigher} />;
+    return (
+      <Fragment>
+        <Card movieDataLower={movieDataHigher} />
+        <Link to={`${match.url}/${movieDataHigher.id}`}>
+          {movieDataHigher.id}
+        </Link>
+      </Fragment>
+    );
   });
-  return moviesListFormated;
+
+  return (
+      <Router>
+      <Switch>
+        <Route exact path={match.path}>
+          {moviesListFormated}
+        </Route>
+        <Route path={`${match.path}/:movieID`}>
+          <MovieDetails/>
+        </Route>
+      </Switch>
+      </Router>
+  );
+
+  function MovieDetails() {
+    // methodology note: I have opted to use another API-call as I preferessed this over pushing the state up from moviesListFormated to shared state in order to use in this component
+    const [movieSelected, setMovieSelected] = useState([]);
+    let { movieID } = useParams();
+    const key = "bde60eb3d70191bf80d726a2da4ae238";
+    Axios.get(
+      `https://api.themoviedb.org/3/movie/${movieID};}?api_key=${key}&language=en-US`
+    ).then(res => {
+      const movie = res.data;
+      setMovieSelected(movie);
+    });
+    return (
+      <p>
+        Requested topic ID: {movieID} = {movieSelected.title}
+      </p>
+    );
+  }
 }
+
 export default Results;
